7d669ec1daea00ae1a1b58851cb93e6856954d3e
gillesdemey <gilles.de.mey@gmail.com>
2021-12-09 14:58:42 +0100

    chore: revert fix

--- public/app/features/explore/Flowmetheus/Flow.tsx
+++ public/app/features/explore/Flowmetheus/Flow.tsx
@@ -1,48 +1,26 @@
 import React, { useLayoutEffect, useMemo, useState } from 'react';
-import ReactFlow, { useStoreActions, useStoreState } from 'react-flow-renderer';
+import ReactFlow, { Elements } from 'react-flow-renderer';
 
-import { layout, Direction } from './hooks/useLayout';
+import { layout } from './hooks/useLayout';
 import { toElements } from './hooks/usePromQlParser';
 
 interface FlowProps {
   query: string;
 }
 
-interface AutoLayoutProps {
-  direction: Direction;
-}
-
-const AutoLayout = ({ direction }: AutoLayoutProps) => {
-  const [didLayout, setDidLayout] = useState<boolean>(false);
-
-  const nodes = useStoreState((state) => state.nodes);
-  const edges = useStoreState((state) => state.edges);
-  const elements = useMemo(() => [...nodes, ...edges], [nodes, edges]);
-
-  const setElements = useStoreActions((actions) => actions.setElements);
+const OverviewFlow = ({ query }: FlowProps) => {
+  const [nodes, edges] = useMemo(() => toElements(query), [query]);
+  const [elements, setElements] = useState<Elements>([...nodes, ...edges]);
 
   useLayoutEffect(() => {
-    if (didLayout || elements.length === 0) {
-      return;
-    }
+    const [nodes, edges] = toElements(query);
 
-    const layoutedElements = layout(elements, direction);
-    setElements(layoutedElements);
-
-    setDidLayout(true);
-  }, [didLayout, direction, edges, elements, nodes, setElements]);
-
-  return null;
-};
-
-const OverviewFlow = ({ query }: FlowProps) => {
-  const [elements, edges] = toElements(query);
+    setElements(layout([...nodes, ...edges], 'RL'));
+  }, [query]);
 
   return (
     <>
-      <ReactFlow elements={[...elements, ...edges]} snapToGrid={false} snapGrid={[15, 15]}>
-        <AutoLayout direction="RL" />
-      </ReactFlow>
+      <ReactFlow elements={elements} snapToGrid={false} snapGrid={[15, 15]}></ReactFlow>
     </>
   );
 };
